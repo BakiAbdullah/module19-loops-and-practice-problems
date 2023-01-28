6b5a126efadf24cf8f12820270350e6bf66b2e74
BakiAbdullah <shatil605@gmail.com>
2023-01-28 20:17:18 +0600

    Added loops

---
+++ reverse-for-while_loop.js
@@ -0,0 +1,20 @@
+// var num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num++
+// }
+
+
+// * Reverse While loop
+// var num = 10;
+// while (num >= 1) {
+//   console.log(num);
+//   num --;
+// }
+
+//? Reverse for loop
+
+for (var i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
