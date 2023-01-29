e36c1117a97ae86b92f0638ee99cf9ff9c857875
BakiAbdullah <shatil605@gmail.com>
2023-01-29 19:58:35 +0600

    Added javascript QNA file

---
+++ javaScript_Interview_ques.js
@@ -0,0 +1,22 @@
+// 1. Declare a variable and 
+var aliName = 'Kodom';
+// change a variables value.
+aliName = 20;
+// console.log(aliName); -------------------------------
+
+// 2. Type of Variable , how can you find out type of a variable?
+var carName = 'Audi';
+var carWeight = 2000;
+var carCanFly = false;
+var carColor;
+// console.log(typeof carColor); =========================
+
+
+// 3. Primitive and non primitive data types :----------------
+//^ Primitive data types:--
+// 1. number
+// 2. NaN
+// 3. Undefined
+
+
+//^ NonPrimitive data types:
